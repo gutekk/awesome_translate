bc918ae24a10be39756d327cb124b674b660a062
gutekk <gutek84@gmail.com>
2015-06-02 09:57:46 +0200

    fix for code climate test reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
-require 'awesome_translate'
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'awesome_translate'
